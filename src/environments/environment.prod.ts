f6db4aa738937177bd3184c77d3ddd4891630f60
Vrushabh Shamkuwar <vrushabh@hudsondata.com>
2022-05-05 13:54:23 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'crud-ang-6977c',
+    appId: '1:380295132073:web:19a69cf7d0c73de3b10ab8',
+    storageBucket: 'crud-ang-6977c.appspot.com',
+    apiKey: 'AIzaSyAtnMxmPpCBCjgrxxF5oYlfa2uufwz9Bak',
+    authDomain: 'crud-ang-6977c.firebaseapp.com',
+    messagingSenderId: '380295132073',
+    measurementId: 'G-7KCLM5XW60',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'crud-ang-6977c',
+    appId: '1:380295132073:web:19a69cf7d0c73de3b10ab8',
+    storageBucket: 'crud-ang-6977c.appspot.com',
+    apiKey: 'AIzaSyAtnMxmPpCBCjgrxxF5oYlfa2uufwz9Bak',
+    authDomain: 'crud-ang-6977c.firebaseapp.com',
+    messagingSenderId: '380295132073',
+    measurementId: 'G-7KCLM5XW60',
+  },
   production: false
 };
 
